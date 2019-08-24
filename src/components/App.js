ccfa54f0e919c911bdf23517ba026dbe4c254317
Michael Winton <wintonmich1@ecs.vuw.ac.nz>
2019-08-24 16:22:03 +1200

    added questions

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import { RegionQuestion } from './RegionQuestion'
+import { GroupSizeQuestion } from './GroupSizeQuestion'
+import { WalkLengthQuestion } from './WalkLengthQuestion'
 
 export const App = () => {
   const [region, setRegion] = useState(null);
+  const [groupSize, setGroupSize] = useState(null);
+  const [walkLength, setWalkLength] = useState(null);
 
   return (
-    <div className="question">
+    <div>
       <RegionQuestion region={region} setRegion={setRegion}/>
+      <GroupSizeQuestion groupSize={groupSize} setGroupSize={setGroupSize}/>
+      <WalkLengthQuestion walkLength={walkLength} setWalkLength={setWalkLength}/>
     </div>
   )
 }
