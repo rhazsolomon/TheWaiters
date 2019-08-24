b594f9e428a7b6822a8e0a1de67e4fa0482a032b
julyseng <julyseng@gmail.com>
2019-08-24 15:50:43 +1200

    add api calls

--- basic/main.js
+++ basic/main.js
@@ -1,11 +1,19 @@
 
 
-function getData() {
-    var theUrl = "https://api.doc.govt.nz/v2/huts"
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.setRequestHeader("x-api-key", "bU5t3d5QOv5V0QNWU36yp5nGDhEtmLDc6UbTfvPM");
-    xmlHttp.send( null );
-    document.getElementById("result").innerHTML = xmlHttp.responseText;
-    return xmlHttp.responseText;
+function myFilteringFunction(jsonObjects, region){
+    var filterData = []
+    jsonObjects.forEach(hutData => {
+        if (hutData.region === region) {
+            filterData.push(hutData)
+        }
+    });
+    return filterData
+}
+
+async function getMeHuts(region) {
+    const headers = { "x-api-key": "bU5t3d5QOv5V0QNWU36yp5nGDhEtmLDc6UbTfvPM" };
+    var data = await fetch("https://api.doc.govt.nz/v2/huts", { headers })
+        .then(res => res.json())
+        .then(jsonObjects => myFilteringFunction(jsonObjects, region))    
+    return data;
 }
