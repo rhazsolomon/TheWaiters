d135254b9519a1b6d2a1609b8d7b6dffb379bdc9
Rhaz Solomon <solomon.rhaz@gmail.com>
2019-08-24 14:07:37 +1200

    Getting data on website

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function getData() {
+    var theUrl = "https://api.doc.govt.nz/v2/huts"
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.setRequestHeader("x-api-key", "bU5t3d5QOv5V0QNWU36yp5nGDhEtmLDc6UbTfvPM");
+    xmlHttp.send( null );
+    document.getElementById("result").innerHTML = xmlHttp.responseText;
+    return xmlHttp.responseText;
+}
